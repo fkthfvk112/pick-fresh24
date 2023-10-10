1e1bf70c10e56a197ffda25134c4737aa542b674
KHM <gusals6360@naver.com>
2023-10-10 09:30:17 +0900

    feat: add productImgUrl in cart

--- emart-fresh-spring/demo/src/main/java/mart/fresh/com/data/dto/CartInfoDto.java
+++ emart-fresh-spring/demo/src/main/java/mart/fresh/com/data/dto/CartInfoDto.java
@@ -8,16 +8,18 @@ public class CartInfoDto {
 	private int cartProductId;
     private int productId;
     private String productTitle;
+    private String productImgUrl;
     private int priceNumber;
     private int productTimeSale;
     private int cartProductQuantity;
     private int storeId;
 
-    public CartInfoDto(int cartId, int cartProductId, int productId, String productTitle, int priceNumber, int productTimeSale, int cartProductQuantity, int storeId) {
+    public CartInfoDto(int cartId, int cartProductId, int productId, String productTitle, String productImgUrl, int priceNumber, int productTimeSale, int cartProductQuantity, int storeId) {
         this.cartId = cartId;
         this.cartProductId = cartProductId;
         this.productId = productId;
     	this.productTitle = productTitle;
+    	this.productImgUrl = productImgUrl;
         this.priceNumber = priceNumber;
         this.productTimeSale = productTimeSale;
         this.cartProductQuantity = cartProductQuantity;
